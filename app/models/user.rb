32be9e204865ea6649e77774ec232b0df3f2f9be
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-13 17:45:58 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :plofile, presence: true
+  validates :profile, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
 end
