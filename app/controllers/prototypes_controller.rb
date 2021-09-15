ac947dcf600297ade1dcb19bf175b611d70e971f
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-15 21:52:29 +0900

    アクセス範囲制限の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :edit]
+  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show] #, :search
 
   def index
     @prototypes = Prototype.all
@@ -46,6 +49,12 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
