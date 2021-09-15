12e3ca613c4bb07b5a629ba6bc450f7e17aff2c1
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-09-15 14:36:16 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,6 +34,12 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
   private
 
   def prototype_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
+  resources :prototypes #, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
